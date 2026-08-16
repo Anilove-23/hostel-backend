b0d794ec041fd9f72095e587399b5ac0ba450ce6
Anilove <anilovejee@gmail.com>
2026-08-17 02:04:05 +0530

    Comit

--- index.js
+++ index.js
@@ -20,7 +20,11 @@ const pool = require("./db/db");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(cors());
+
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
